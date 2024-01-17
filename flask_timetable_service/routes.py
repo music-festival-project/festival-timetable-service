dd22b88c3f66ebb4f074cd023a54da9a80341f62
Mihaela Aleksandrova <74715450+mihaelaaleks@users.noreply.github.com>
2024-01-17 17:39:03 +0100

    added example flexible route for later testing

--- flask_timetable_service/routes.py
+++ flask_timetable_service/routes.py
@@ -1,6 +1,12 @@
 from flask import current_app as app
-
+import requests
 
 @app.route("/")
 def index():
     return "hello this is a flask app"
+
+@app.route('festival/<festival_id>', methods=['GET'])
+def get_festival_timetable(festival_id):
+    # search festival id in mongo document
+    # jsonify this string
+    return f"Product ID: {festival_id}"
