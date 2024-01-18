fe3c6cb0c04f84b363a8927ef4b6d40ce6385843
Mihaela Aleksandrova <74715450+mihaelaaleks@users.noreply.github.com>
2024-01-18 11:59:33 +0100

    added flexible routing for festivals

--- flask_timetable_service/routes.py
+++ flask_timetable_service/routes.py
@@ -1,12 +1,13 @@
 from flask import current_app as app
 import requests
+import requests
+import json
 
 @app.route("/")
 def index():
     return "hello this is a flask app"
 
-@app.route('festival/<festival_id>', methods=['GET'])
+@app.route('/festival/<festival_id>', methods=['GET'])
 def get_festival_timetable(festival_id):
-    # search festival id in mongo document
-    # jsonify this string
-    return f"Product ID: {festival_id}"
+    
+    ...
